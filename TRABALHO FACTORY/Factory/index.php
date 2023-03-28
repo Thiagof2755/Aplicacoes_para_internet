ede27497f0f8f03988e4e228a0920b18e2fa2d1c
Thiago Alves <80608138+Thiagof2755@users.noreply.github.com>
2023-03-28 10:14:41 -0300

    update

--- TRABALHO FACTORY/Factory/index.php
+++ TRABALHO FACTORY/Factory/index.php
@@ -3,7 +3,7 @@
 require_once 'PessoaFactory.php';
 $factory = new PessoaFactory();
 
-$Pesoa1 = $factory->createPerson('Thiago alves','34 97628585','Casa','Gestor');
+$Pesoa1 = $factory->criarPessoa('Thiago alves','34 97628585','Casa','Gestor');
 $Pesoa1->pagamento();
 
 echo $Pesoa1->getType() . "\n"; 
